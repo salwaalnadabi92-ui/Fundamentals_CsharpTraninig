5fcbd8efced2a6cef335ce10fb2d24841eeeef83
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-05-16 13:21:24 +0400

    add   all variable declarations

--- Program.cs
+++ Program.cs
@@ -1,10 +1,76 @@
-﻿namespace Fundamentals_CsharpTraninig
+﻿namespace ClinicSystem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            // Capacity constants
+
+            const int MAX_PATIENTS = 3;
+            const int MAX_DOCTORS = 2;
+            const int MAX_APPOINTMENTS = 3;
+
+
+            // Patient slots
+
+            string p1Name = ""; int p1Age = 0; string p1Phone = ""; bool p1Active = false;
+
+            string p2Name = ""; int p2Age = 0; string p2Phone = ""; bool p2Active = false;
+
+            string p3Name = ""; int p3Age = 0; string p3Phone = ""; bool p3Active = false;
+
+
+            int patientCount = 0;
+
+
+
+            // Doctor slots
+
+
+            string d1Name = "", d1Spec = ""; double d1Fee = 0; bool d1Active = false;
+
+            string d2Name = "", d2Spec = ""; double d2Fee = 0; bool d2Active = false;
+
+            int doctorCount = 0;
+
+
+            // Appointment slots
+
+            string a1Patient = "";
+            string a1Doctor = "";
+            string a1Date = "";
+            string a1Status = "";
+            bool a1Active = false;
+
+
+            string a2Patient = "";
+            string a2Doctor = "";
+            string a2Date = "";
+            string a2Status = "";
+            bool a2Active = false;
+
+
+            string a3Patient = "";
+            string a3Doctor = "";
+            string a3Date = "";
+            string a3Status = "";
+            bool a3Active = false;
+
+            int appointmentCount = 0;
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
