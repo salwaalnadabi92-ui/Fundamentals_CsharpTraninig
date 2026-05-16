0679a69f60b22e5e8ade0516481f30f9f20e1d82
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-05-16 13:05:18 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Fundamentals_CsharpTraninig
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
